1fcb2b09b81db8b0099ddf9095bd8b08309bc859
Mark Erenberg <markkerenberg@gmail.com>
2019-09-21 11:00:31 -0400

    Add files via upload

---
+++ Skeleton.py
@@ -0,0 +1,37 @@
+##
+## =======================================================
+## Branch Wait Times Hack 
+## ELevate Tech Jam 2019
+## =======================================================
+##
+
+##
+## =======================================================
+## Mark Erenberg 
+## Toxic Comment Classification Challenge
+## =======================================================
+##
+
+# Objective: Create a model which predicts a probability of each type of toxicity for each comment.
+
+import os
+import sys
+import gc
+import time
+import pandas as pd
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+import re
+
+##### Data Loading #####
+os.chdir('C:\\Users\\marke\\Downloads\\Elevate Tech Jam')
+
+
+##### Data Cleaning #####
+
+
+##### Feature Engineering #####
+
+##### Model Fitting #####
+  
